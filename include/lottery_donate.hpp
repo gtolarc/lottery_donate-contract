fa2cb68858ad661aeff6872c461224c3025bda42
gtolarc <gtolarc@gmail.com>
2019-03-13 15:20:00 +0900

    make a contract structure

--- include/lottery_donate.hpp
+++ include/lottery_donate.hpp
@@ -1,11 +1,47 @@
 #include <eosiolib/eosio.hpp>
+
 using namespace eosio;
 
 CONTRACT lottery_donate : public contract {
    public:
-      using contract::contract;
+    lottery_donate(name receiver, name code, datastream<const char*> ds)
+        : contract(receiver, code, ds),
+          _balances(receiver, receiver.value),
+          _games(receiver, receiver.value),
+          _histories(receiver, receiver.value) {}
 
-      ACTION hi( name nm );
+    ACTION join(name user);
+    ACTION refundinit(uint64_t amount);
 
-      using hi_action = action_wrapper<"hi"_n, &lottery_donate::hi>;
+    TABLE balance {
+        uint64_t num;
+        name holder;
+        uint64_t primary_key() const { return num; }
+    };
+
+    TABLE game {
+        uint64_t amount;
+        uint8_t count;
+        uint64_t primary_key() const { return amount; }
+    };
+
+    TABLE history {
+        uint64_t seq;
+        name holder;
+        uint64_t amount;
+        uint8_t count;
+        uint64_t primary_key() const { return seq; }
+    };
+
+    typedef eosio::multi_index<"balances"_n, balance> balances;
+    typedef eosio::multi_index<"games"_n, game> games;
+    typedef eosio::multi_index<"histories"_n, history> histories;
+
+    using join_action = action_wrapper<"join"_n, &lottery_donate::join>;
+    using refundinit_action = action_wrapper<"refundinit"_n, &lottery_donate::refundinit>;
+
+   private:
+    balances _balances;
+    games _games;
+    histories _histories;
 };
--- src/lottery_donate.cpp
+++ src/lottery_donate.cpp
@@ -1,7 +1,6 @@
 #include <lottery_donate.hpp>
-ACTION lottery_donate::hi( name nm ) {
-   /* fill in action body */
-   print_f("Name : %\n",nm);
-}
 
-EOSIO_DISPATCH( lottery_donate, (hi) )
+ACTION lottery_donate::join(name user) {}
+ACTION lottery_donate::refundinit(uint64_t amount) {}
+
+EOSIO_DISPATCH(lottery_donate, (join)(refundinit))
